fcb089ef3fc7716f3211418a103d714e4e8ec4ed
prasa508 <prasad.prasad@kpn.com>
2020-12-31 01:16:53 +0100

    Code Cleanup

--- src/test/java/com/typicode/jsonph/base/TestBase.java
+++ src/test/java/com/typicode/jsonph/base/TestBase.java
@@ -5,14 +5,8 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.testng.annotations.BeforeClass;
 
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 
 public class TestBase {
-	
-	public static RequestSpecification httpRequest;
-	public static Response response;
-	
 	
 	public Logger logger;
 	
@@ -20,7 +14,7 @@ public class TestBase {
 	public void setup(){
 		
 		logger=Logger.getLogger("Json Typicode");//added Logger
-		PropertyConfigurator.configure("Log4j.properties"); //added logger
+		PropertyConfigurator.configure("log4j.properties"); //added logger
 		logger.setLevel(Level.DEBUG);
 		
 	}
